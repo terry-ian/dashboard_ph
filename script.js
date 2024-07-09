ea911dc35ef6f07df24e5de7ea3fda00e6644732
terry-ian <39936938+terry-ian@users.noreply.github.com>
2024-07-09 16:47:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ function login() {
     
     // 簡單的帳號密碼驗證（實際應用中請使用更安全的方法）
     if (username === 'torin' && password === 'qwe123') {
+        localStorage.setItem('loggedIn', 'true');
         window.location.href = 'dashboard.html';
     } else {
         alert('帳號或密碼錯誤');
+    }
+}
+
+function checkLogin() {
+    if (localStorage.getItem('loggedIn') !== 'true') {
+        window.location.href = 'index.html';
     }
 }
